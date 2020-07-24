482df05d3f17dfea50b213ddefe789821df30fea
DarkPro1337 <darkprothekun@gmail.com>
2020-07-24 13:32:44 +0300

    Initial commit

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_new_game_pressed():
+	pass #TODO
+
+func _on_settings_pressed():
+	pass #TODO
+
+
+func _on_scores_pressed():
+	pass #TODO
+
+func _on_exit_pressed():
+	get_tree().quit()
