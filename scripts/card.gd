d43191d043228e4e7ed6d1f2779edc023a00d870
Artem Darkov <darkprothekun@gmail.com>
2020-08-24 18:35:01 +0300

    minor updates and some progress

---
+++ scripts/card.gd
@@ -0,0 +1,40 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_card_input(event):
+	if Input.is_action_pressed("ui_lmb"):
+		pass #TODO
+	if Input.is_action_pressed("ui_rmb"):
+		pass #TODO
+
+func damage_tower(hp):
+	pass #TODO
+
+func damage_wall(hp):
+	pass #TODO
+
+func heal_tower(hp):
+	pass #TODO
+
+func heal_wall(hp):
+	pass #TODO
+
+func add_quarry(num):
+	pass #TODO
+
+func add_bricks(num):
+	pass #TODO
+
+func add_magic(num):
+	pass #TODO
+
+func add_gems(num):
+	pass #TODO
+
+func add_dungeons(num):
+	pass #TODO
+
+func add_recruits(num):
+	pass #TODO
---
+++ scripts/table.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var turn
+
+# DEFAULT TOWER AND WALL HP
+var player_tower_hp = 50
+var player_wall_hp = 50
+
+var enemy_tower_hp = 50
+var enemy_wall_hp = 50
+
+# DEFAULT RESOURCES
+var player_quarry = 5
+var player_bricks = 20
+var player_magic = 3
+var player_gems = 10
+var player_dungeon = 5
+var player_recruits = 20
+
+var enemy_quarry = 5
+var enemy_bricks = 20
+var enemy_magic = 3
+var enemy_gems = 10
+var enemy_dungeon = 5
+var enemy_recruits = 20
+
+func _ready():
+	pass
